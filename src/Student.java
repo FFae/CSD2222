29cbc4e7721605230890e4447339205ef9d7715d
FF173 <FF173@PC-00191544.uni.mdx.ac.uk>
2014-11-03 17:44:54 +0000

    Student and Test classes

---
+++ src/Student.java
@@ -0,0 +1,86 @@
+
+
+/*
+ * Fernando Fae
+ * M00483894
+ */
+
+
+public class Student {
+
+	private String  name;       // name has	default	value null
+	private int     age;        // student name
+	private String  programme;  //student programme
+	private char    gender;     //student gender
+	
+	
+	// construct a student
+	public Student(String name, int age, String programme, char gender){
+		
+		this.name      = name       ;
+		this.age       = age	    ;	
+		this.programme = programme  ;
+		this.gender    = gender     ;
+	}
+	
+	public void displayStudentDetails() {
+		
+		if (this.programme.contentEquals("Computing")){
+		
+			System.out.println(	"Name: "      + name + 
+					            " Age: "      + age + 
+					            " Programme: " + programme + 
+					            " Gender: "    + gender +
+					            "\n")	;
+		}
+		else{
+			
+			System.out.println(	"Name: "      + name + 
+					            " Age: "      + age + 
+					            " Programme: Other Programme" +  
+					            " Gender: "    + gender +
+					            "\n")	;
+		}
+	}
+	
+	public String getProgramme(){
+		return programme;
+	}
+	
+	public void setProgramme(String programme){
+		this.programme = programme;
+	}
+	
+	public char getGender(){
+		return gender;
+	}
+	
+	public void setGender(char gender){
+		this.gender = gender;
+	}
+	
+	public int getAge(){
+		return age;
+	}
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/testStudent.java
@@ -0,0 +1,18 @@
+
+
+public class testStudent {
+	
+	public static void main(String[] args){
+		
+		Student firstStudent = new Student("John", 19, "Biology", 'M');
+		firstStudent.displayStudentDetails();
+		
+		Student secondStudent = new Student("Johnny", 29, "Biology", 'M');
+		secondStudent.displayStudentDetails();
+		
+		Student thirdStudent = new Student("Fernando", 23, "Computing", 'M');
+		thirdStudent.displayStudentDetails();
+	}
+}
+
+
